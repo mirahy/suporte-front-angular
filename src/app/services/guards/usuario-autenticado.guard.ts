eeb503db820056771900819ba32de5bcc6261e7c
Mirahy Ribeiro Branco Fonseca <mirahyfonseca@ufgd.edu.br>
2023-09-12 19:31:34 -0400

    exibir token somenete em ambiente de desenvolvimento

--- src/app/services/guards/usuario-autenticado.guard.ts
+++ src/app/services/guards/usuario-autenticado.guard.ts
@@ -1,6 +1,7 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { UsuarioService } from '../usuario.service';
 import { inject } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 
 export const usuarioAutenticadoGuard: CanActivateFn = (route, state) => {
@@ -8,7 +9,8 @@ export const usuarioAutenticadoGuard: CanActivateFn = (route, state) => {
   const usuarioService: UsuarioService = inject(UsuarioService)
   const router: Router = inject(Router)
   if(usuarioService.logado){
-    console.log(usuarioService.obterTokenUsuario)
+    if(!environment.production)
+        console.log(usuarioService.obterTokenUsuario)
     return true;
   }
   router.navigate(['login'])
