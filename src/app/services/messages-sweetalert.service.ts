f9613a9e8b0b87ed41777c6a1d0144fec5858b1c
Mirahy Ribeiro Branco Fonseca <mirahyfonseca@ufgd.edu.br>
2023-09-13 19:10:13 -0400

    Add paramentro msg a função msgHttp, alterando metodo get do service periodo-letivos

--- src/app/services/messages-sweetalert.service.ts
+++ src/app/services/messages-sweetalert.service.ts
@@ -26,22 +26,34 @@ export class MessagesSweetalertService {
     })
   }
 
-  msgHttp(response:any){
+  msgHttp(response:any, msg?:string){
     switch (response.status) {
       case 200:
-        let msg200_1 = response.data ? response.data.msg : " "
-        let msg200_2 = ""
+        let msg200_1 = 'Tudo ok!'
+        let msg200_2 = msg ? msg : response.data ? response.data.msg : " "
         this.successMsg(msg200_1, msg200_2)
         break;
 
+      case 400:
+        let msg400_1 = "Erro de execução!!"
+        let msg400_2 = msg ? msg : "Erro no processamento dos dados!, code: 400"
+        this.errorMsg(msg400_1, msg400_2, 'home', 'Home')
+        break;
+
       case 401:
-        let msg401_1 = "Sem Acesso!"
-        let msg401_2 = "Usuário não autorizado!"
+        let msg401_1 = "Sem Acesso!!"
+        let msg401_2 = msg ? msg : "Usuário não autorizado!, code: 401"
         this.errorMsg(msg401_1, msg401_2, 'login', 'Login')
+        break;
+
+      case 404:
+        let msg404_1 = "!!!"
+        let msg404_2 = msg ? msg : "Página não encontada!, code: 404"
+        this.errorMsg(msg404_1, msg404_2, 'home', 'Home')
         break;
     
       default:
-        this.errorMsg(response.message, 'login', 'Login')
+        this.errorMsg(msg ? msg : response.message, 'login', 'Login')
         break;
     }
   }
--- src/app/services/periodo-letivos.service.ts
+++ src/app/services/periodo-letivos.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ArrayIndexador } from '../shared/array-indexador';
 import { PeriodoLetivo } from '../components/pages/periodo-letivos/periodo-letivo';
+import { ApiRequestsService } from './api-requests.service';
+import { MessagesSweetalertService } from './messages-sweetalert.service';
+import { AxiosResponse } from 'axios';
 
 @Injectable(
     //{providedIn: 'root'}
@@ -14,24 +17,28 @@ export class PeriodoLetivosService {
     periodoLetivosKeyIndex: ArrayIndexador<PeriodoLetivo> | any;
     periodoLetivosNameIndex: ArrayIndexador<PeriodoLetivo> | any;
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private api: ApiRequestsService, private msg: MessagesSweetalertService) { }
 
-    getPeriodoLetivos() {
-        return this.http.get("/periodo-letivos/all").toPromise()
-            .then((response:any) => {
-                var pls = response.json();
-                this.periodoLetivos = [];
-                this.periodoLetivosIndex = {};
-                for (var i in pls) {
-                    var pl = new PeriodoLetivo(pls[i]);
-                    this.periodoLetivos.push(pl);
-                    this.periodoLetivosIndex[pl.id] = i;
-                }
-                this.periodoLetivosIdIndex = new ArrayIndexador<PeriodoLetivo>(this.periodoLetivos);
-                this.periodoLetivosKeyIndex = new ArrayIndexador<PeriodoLetivo>(this.periodoLetivos,'id_sigecad');
-                this.periodoLetivosNameIndex = new ArrayIndexador<PeriodoLetivo>(this.periodoLetivos,'nome');
-                return this.periodoLetivos;
-            });
+    async getPeriodoLetivos() {
+        try {
+            const response =  await this.api.get("periodo-letivos/all")
+            console.log(response)
+            var pls = response.data;
+            this.periodoLetivos = [];
+            this.periodoLetivosIndex = {};
+            for (var i in pls) {
+                var pl = new PeriodoLetivo(pls[i]);
+                this.periodoLetivos.push(pl);
+                this.periodoLetivosIndex[pl.id] = i;
+            }
+            this.periodoLetivosIdIndex = new ArrayIndexador<PeriodoLetivo>(this.periodoLetivos);
+            this.periodoLetivosKeyIndex = new ArrayIndexador<PeriodoLetivo>(this.periodoLetivos,'id_sigecad');
+            this.periodoLetivosNameIndex = new ArrayIndexador<PeriodoLetivo>(this.periodoLetivos,'nome');
+            return this.periodoLetivos; 
+        } catch (response:any) {
+            return this.msg.msgHttp(response.response)
+        }
+            
     }
 
     getPeriodoLetivosSigecad():Promise<Array<PeriodoLetivo>> {
